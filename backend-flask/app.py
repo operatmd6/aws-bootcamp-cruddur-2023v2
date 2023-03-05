f556be10f034e43f2c6938630b907514f21eada5
operatmd6 <coxcombred@protonmail.com>
2023-03-05 17:57:59 +0000

    rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,14 @@ from services.create_message import *
 from services.show_activity import *
 from services.notifications_activities import *
 
+#rOLLBAR
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+import os
+
+
+
 
 # Honeycomb
 from opentelemetry import trace
@@ -48,6 +56,27 @@ provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
+app = Flask(__name__)
+
+
+#Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 #Xray
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
@@ -59,7 +88,7 @@ provider.add_span_processor(simple_processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
-app = Flask(__name__)
+
 XRayMiddleware(app, xray_recorder)
 # Honeycomb
 # Initialize automatic instrumentation with Flask
@@ -82,6 +111,11 @@ cors = CORS(
 #    timestamp = strftime('[%Y-%b-%d %H:%M]')
 #    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #    return response
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
